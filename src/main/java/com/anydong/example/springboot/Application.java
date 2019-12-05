9bb25badc3e0badd9ce0b24f6cf201df03fb9f87
liuzhaowei <liuzhaowei55@gmail.com>
2019-12-05 11:18:28 +0800

    feat: 设置默认时区

--- src/main/java/com/anydong/example/springboot/Application.java
+++ src/main/java/com/anydong/example/springboot/Application.java
@@ -3,9 +3,12 @@ package com.anydong.example.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 /**
  * Application
- * 
+ *
  * @author Where
  * @date 2019/10/31
  */
@@ -13,5 +16,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
+        System.setProperty("tomcat.util.http.parser.HttpParser.requestTargetAllow", "|{}");
     }
 }
