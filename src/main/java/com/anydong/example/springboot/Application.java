d76cc13ba723c6abd6fc12ae60c37d02a890aabf
liuzhaowei <liuzhaowei55@gmail.com>
2019-12-05 11:22:25 +0800

    fix(doc): 添加 init 注释

--- src/main/java/com/anydong/example/springboot/Application.java
+++ src/main/java/com/anydong/example/springboot/Application.java
@@ -18,6 +18,10 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    /**
+     * 设置默认时区为：Asia/Shanghai
+     * 设置 URL 中可传入的特殊字符
+     */
     @PostConstruct
     public void init() {
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
