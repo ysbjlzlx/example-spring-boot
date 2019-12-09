374b7e149ecef480900d151cf2966d93fd05019a
LIU Zhaowei <liuzhaowei55@gmail.com>
2019-12-10 01:30:17 +0800

    update

--- src/main/java/com/anydong/example/springboot/controller/PlaceController.java
+++ src/main/java/com/anydong/example/springboot/controller/PlaceController.java
@@ -1,25 +1,32 @@
-package com.anydong.example.springboot;
+package com.anydong.example.springboot.controller;
 
+import com.alibaba.fastjson.JSON;
 import org.geotools.geojson.geom.GeometryJSON;
-import org.junit.Test;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.Point;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.io.StringWriter;
 
-@SpringBootTest
-public class GeoJsonTests {
-    @Test
-    public void pointToGeometryJson() throws IOException {
+/**
+ * @author anydong
+ */
+@RestController
+@RequestMapping(value = "/place", produces = "application/json; charset=utf-8")
+public class PlaceController {
+    @GetMapping
+    public Object getGeoJson() throws IOException {
+
         GeometryFactory geometryFactory = new GeometryFactory();
         Point point = geometryFactory.createPoint(new Coordinate(113, 22));
 
         StringWriter stringWriter = new StringWriter();
         GeometryJSON geometryJSON = new GeometryJSON();
         geometryJSON.writePoint(point, stringWriter);
-        System.out.println(stringWriter);
+        return JSON.parse(stringWriter.toString());
     }
 }
