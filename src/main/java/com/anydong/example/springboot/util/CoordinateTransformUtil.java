9517c2d6134a3d6963c3c21abc8ab8fff33d2eb2
LIU Zhaowei <liuzhaowei55@gmail.com>
2019-12-10 23:50:16 +0800

    feat: 完善了坐标转换的测试

--- src/main/java/com/anydong/example/springboot/util/CoordinateTransformUtil.java
+++ src/main/java/com/anydong/example/springboot/util/CoordinateTransformUtil.java
@@ -5,12 +5,12 @@ import org.springframework.stereotype.Component;
 /**
  * @author anydong
  * 原始代码来自这里：https://github.com/wandergis/coordtransform/blob/master/index.js
+ * WGS84 为一种大地坐标系，也是目前广泛使用的GPS全球卫星定位系统使用的坐标系。
+ * GCJ02 又称火星坐标系，是由中国国家测绘局制订的地理信息系统的坐标系统。由WGS84坐标系经加密后的坐标系。
+ * BD09 为百度坐标系，在GCJ02坐标系基础上再次加密。其中bd09ll表示百度经纬度坐标，bd09mc表示百度墨卡托米制坐标。
  */
 @Component
 public class CoordinateTransformUtil {
-    public enum CoordinateType {
-        WGS84, GCJ02, BD09
-    }
     /**
      * PI π
      * //
@@ -26,23 +26,6 @@ public class CoordinateTransformUtil {
     private final double PI = 3.1415926535897932384626;
     private final double a = 6378245.0;
     private final double ee = 0.00669342162296594323;
-
-    public double[] gcj02ToWgs84(double lng, double lat) {
-        if (outOfChina(lng, lat)) {
-            return new double[]{lng, lat};
-        }
-        double dlat = transformLat(lng - 105.0, lat - 35.0);
-        double dlng = transformLng(lng - 105.0, lat - 35.0);
-        double radlat = lat / 180.0 * PI;
-        double magic = Math.sin(radlat);
-        magic = 1 - ee * magic * magic;
-        double sqrtmagic = Math.sqrt(magic);
-        dlat = (dlat * 180.0) / ((a * (1 - ee)) / (magic * sqrtmagic) * PI);
-        dlng = (dlng * 180.0) / (a / sqrtmagic * Math.cos(radlat) * PI);
-        double mglat = lat + dlat;
-        double mglng = lng + dlng;
-        return new double[]{lng * 2 - mglng, lat * 2 - mglat};
-    }
 
     public double[] wgs84ToGcj02(double lng, double lat) {
         if (outOfChina(lng, lat)) {
@@ -61,23 +44,53 @@ public class CoordinateTransformUtil {
         return new double[]{mglng, mglat};
     }
 
+    public double[] wgs84ToBd09(double lng, double lat) {
+        double[] gcj02 = this.wgs84ToGcj02(lng, lat);
+        return this.gcj02ToBd09(gcj02[0], gcj02[1]);
+    }
+
+
+    public double[] gcj02ToWgs84(double lng, double lat) {
+        if (outOfChina(lng, lat)) {
+            return new double[]{lng, lat};
+        }
+        double dlat = transformLat(lng - 105.0, lat - 35.0);
+        double dlng = transformLng(lng - 105.0, lat - 35.0);
+        double radlat = lat / 180.0 * PI;
+        double magic = Math.sin(radlat);
+        magic = 1 - ee * magic * magic;
+        double sqrtmagic = Math.sqrt(magic);
+        dlat = (dlat * 180.0) / ((a * (1 - ee)) / (magic * sqrtmagic) * PI);
+        dlng = (dlng * 180.0) / (a / sqrtmagic * Math.cos(radlat) * PI);
+        double mglat = lat + dlat;
+        double mglng = lng + dlng;
+        return new double[]{lng * 2 - mglng, lat * 2 - mglat};
+    }
+
+
     public double[] gcj02ToBd09(double lng, double lat) {
-        var z = Math.sqrt(lng * lng + lat * lat) + 0.00002 * Math.sin(lat * x_PI);
-        var theta = Math.atan2(lat, lng) + 0.000003 * Math.cos(lng * x_PI);
-        var bdLng = z * Math.cos(theta) + 0.0065;
-        var bdLat = z * Math.sin(theta) + 0.006;
+        double z = Math.sqrt(lng * lng + lat * lat) + 0.00002 * Math.sin(lat * x_PI);
+        double theta = Math.atan2(lat, lng) + 0.000003 * Math.cos(lng * x_PI);
+        double bdLng = z * Math.cos(theta) + 0.0065;
+        double bdLat = z * Math.sin(theta) + 0.006;
         return new double[]{bdLng, bdLat};
     }
 
+    public double[] bd09ToWgs84(double lng, double lat) {
+        double[] gcj02 = bd09ToGcj02(lng, lat);
+        return gcj02ToWgs84(gcj02[0], gcj02[1]);
+    }
+
     public double[] bd09ToGcj02(double lng, double lat) {
-        var x = lng - 0.0065;
-        var y = lat - 0.006;
-        var z = Math.sqrt(x * x + y * y) - 0.00002 * Math.sin(y * x_PI);
-        var theta = Math.atan2(y, x) - 0.000003 * Math.cos(x * x_PI);
-        var gcjLng = z * Math.cos(theta);
-        var gcgLat = z * Math.sin(theta);
+        double x = lng - 0.0065;
+        double y = lat - 0.006;
+        double z = Math.sqrt(x * x + y * y) - 0.00002 * Math.sin(y * x_PI);
+        double theta = Math.atan2(y, x) - 0.000003 * Math.cos(x * x_PI);
+        double gcjLng = z * Math.cos(theta);
+        double gcgLat = z * Math.sin(theta);
         return new double[]{gcjLng, gcgLat};
     }
+
 
     private double transformLng(double lng, double lat) {
         double ret = 300.0 + lng + 2.0 * lat + 0.1 * lng * lng + 0.1 * lng * lat + 0.1 * Math.sqrt(Math.abs(lng));
--- src/test/java/com/anydong/example/springboot/util/CoordinateTransformUtilTests.java
+++ src/test/java/com/anydong/example/springboot/util/CoordinateTransformUtilTests.java
@@ -5,23 +5,81 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class CoordinateTransformUtilTests {
-    private CoordinateTransformUtil transformUtil = new CoordinateTransformUtil();
+    CoordinateTransformUtil coordinateTransformUtil = new CoordinateTransformUtil();
 
-    @Test
-    public void gcj02ToWgs84Test() {
-        double lng = 113.0;
-        double lat = 22.0;
-        double[] res = this.transformUtil.gcj02ToWgs84(lng, lat);
-        System.out.println(res[0]);
-        System.out.println(res[1]);
-    }
-
+    /**
+     * 高德 Web 服务 API 坐标转换服务
+     * Doc: https://lbs.amap.com/api/webservice/guide/api/convert
+     */
     @Test
     public void wgs84ToGcj02Test() {
-        double lng = 116.481499;
-        double lat = 39.990475;
-        double[] res = this.transformUtil.wgs84ToGcj02(lng, lat);
-        System.out.println(res[0]);
-        System.out.println(res[1]);
+        double lng = 116.481499; //116.487585177952
+        double lat = 39.990475; //39.991754014757
+        double[] gcj02 = this.coordinateTransformUtil.wgs84ToGcj02(lng, lat);
+        System.out.println(gcj02[0]);
+        System.out.println(gcj02[1]);
     }
+
+    /**
+     * 校验：https://tool.lu/coordinate/
+     */
+    @Test
+    public void gcj02ToWgs84Test() {
+        double lng = 116.487585177952; //116.481499
+        double lat = 39.991754014757; //39.990475
+        double[] wgs84 = this.coordinateTransformUtil.gcj02ToWgs84(lng, lat);
+        System.out.println(wgs84[0]);
+        System.out.println(wgs84[1]);
+    }
+
+    /**
+     * 百度 Web 服务 API 坐标转换服务
+     * Doc: http://lbs.baidu.com/index.php?title=webapi/guide/changeposition
+     * URL: https://api.map.baidu.com/geoconv/v1/?coords=116.481499,39.990475&from=1&to=5&ak=ACcbe2e69d6aa971c782d0da8cc560da
+     */
+    @Test
+    public void wgs84ToBd09Test() {
+        double lng = 116.481499; //116.49412480447363
+        double lat = 39.990475; //39.997721127970269
+        double[] bd09 = this.coordinateTransformUtil.wgs84ToBd09(lng, lat);
+        System.out.println(bd09[0]);
+        System.out.println(bd09[1]);
+    }
+
+    /**
+     * 校验：https://tool.lu/coordinate/
+     */
+    @Test
+    public void bd09ToWgs84Test() {
+        double lng = 116.49412480447363; //116.481499
+        double lat = 39.997721127970269; //39.990475
+        double[] wgs84 = this.coordinateTransformUtil.bd09ToWgs84(lng, lat);
+        System.out.println(wgs84[0]);
+        System.out.println(wgs84[1]);
+    }
+
+    /**
+     * url: https://api.map.baidu.com/geoconv/v1/?coords=116.481499,39.990475&from=3&to=5&ak=ACcbe2e69d6aa971c782d0da8cc560da
+     */
+    @Test
+    public void gcj02ToBd09Test() {
+        double lng = 116.481499; //116.48806956500994
+        double lat = 39.990475; //39.9963310359532
+        double[] bd09 = this.coordinateTransformUtil.gcj02ToBd09(lng, lat);
+        System.out.println(bd09[0]);
+        System.out.println(bd09[1]);
+    }
+    
+    /**
+     * 校验：https://tool.lu/coordinate/
+     */
+    @Test
+    public void bd09ToGcj02Test() {
+        double lng = 116.48806956500994; //116.481499
+        double lat = 39.9963310359532; //39.990475
+        double[] gcj02 = this.coordinateTransformUtil.bd09ToGcj02(lng, lat);
+        System.out.println(gcj02[0]);
+        System.out.println(gcj02[1]);
+    }
+
 }
