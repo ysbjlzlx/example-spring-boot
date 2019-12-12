63084f7e6ec8bcff5bdc6f75835bce16b8a5b17b
LIU Zhaowei <liuzhaowei55@gmail.com>
2019-12-12 22:24:37 +0800

    feat: scheduled

---
+++ src/main/java/com/anydong/example/springboot/task/LogTask.java
@@ -0,0 +1,21 @@
+package com.anydong.example.springboot.task;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * @author anydong
+ */
+@Component
+public class LogTask {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LogTask.class);
+
+    @Scheduled(fixedDelay = 10000)
+    public void printDatetime() {
+        LOGGER.info((new Date()).toString());
+    }
+}
