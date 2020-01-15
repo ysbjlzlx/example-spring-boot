57d9a64f077a93bb5d8339e9fadba3859f7cc737
liuzhaowei <liuzhaowei55@gmail.com>
2020-01-15 14:39:41 +0800

    fix: fastjson utf-8 编码

--- src/main/java/com/anydong/example/springboot/config/WebMvcConfigurerImpl.java
+++ src/main/java/com/anydong/example/springboot/config/WebMvcConfigurerImpl.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 /**
@@ -22,9 +23,12 @@ public class WebMvcConfigurerImpl implements WebMvcConfigurer {
         FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
         FastJsonConfig config = new FastJsonConfig();
         config.setSerializerFeatures(
+                SerializerFeature.WriteNullListAsEmpty,
                 SerializerFeature.WriteMapNullValue,
-                SerializerFeature.UseISO8601DateFormat
+                SerializerFeature.UseISO8601DateFormat,
+                SerializerFeature.DisableCircularReferenceDetect
         );
+        config.setCharset(StandardCharsets.UTF_8);
         converter.setFastJsonConfig(config);
         converters.add(0, converter);
     }
