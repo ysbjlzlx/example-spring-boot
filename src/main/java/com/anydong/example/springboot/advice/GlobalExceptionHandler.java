07c461d0cc63c080998b5cee619e458ec7f27125
where.liu <where.liu@huolala.cn>
2021-07-15 11:08:36 +0800

    feat: save

--- src/main/java/com/anydong/example/springboot/advice/GlobalExceptionHandler.java
+++ src/main/java/com/anydong/example/springboot/advice/GlobalExceptionHandler.java
@@ -13,11 +13,18 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.validation.ConstraintViolationException;
 import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    /**
+     * 处理Get请求中 使用@Valid 验证路径中请求实体校验失败后抛出的异常
+     *
+     * @param e BindException
+     * @return CustomResponseDto
+     */
     @ExceptionHandler(BindException.class)
     @ResponseStatus(code = HttpStatus.UNPROCESSABLE_ENTITY)
     public CustomResponseDto bindExceptionHandler(BindException e) {
@@ -30,6 +37,32 @@ public class GlobalExceptionHandler {
         return customResponseDTO;
     }
 
+    /**
+     * 处理请求参数格式错误 @RequestParam上validate失败后抛出的异常是javax.validation.ConstraintViolationException
+     *
+     * @param e ConstraintViolationException
+     * @return CustomResponseDto
+     */
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(code = HttpStatus.UNPROCESSABLE_ENTITY)
+    public CustomResponseDto constraintViolationExceptionHandler(ConstraintViolationException e) {
+        ValidatedResult validatedResult = new ValidatedResult(false);
+        e.getConstraintViolations().forEach(constraintViolation -> {
+            String field = constraintViolation.getPropertyPath().toString();
+            validatedResult.addError(field, constraintViolation.getMessage());
+        });
+        CustomResponseDto customResponseDTO = new CustomResponseDto(100422, "");
+        customResponseDTO.setData(validatedResult.getErrors());
+        return customResponseDTO;
+
+    }
+
+    /**
+     * 处理请求参数格式错误 @RequestBody上validate失败后抛出的异常是MethodArgumentNotValidException异常
+     *
+     * @param e MethodArgumentNotValidException
+     * @return CustomResponseDto
+     */
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(code = HttpStatus.UNPROCESSABLE_ENTITY)
     public CustomResponseDto methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
@@ -42,6 +75,12 @@ public class GlobalExceptionHandler {
         return customResponseDTO;
     }
 
+    /**
+     * 处理请求参数格式错误 @RequestBody JSON 解析错误时抛出
+     *
+     * @param e HttpMessageNotReadableException
+     * @return CustomResponseDto
+     */
     @ExceptionHandler(HttpMessageNotReadableException.class)
     @ResponseStatus(code = HttpStatus.UNPROCESSABLE_ENTITY)
     public CustomResponseDto httpMessageNotReadableExceptionHandler(HttpMessageNotReadableException e) {
