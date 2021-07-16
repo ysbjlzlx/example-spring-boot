68be5133499fb6ecef8538db69a81cde84210017
where.liu <where.liu@huolala.cn>
2021-07-16 09:17:00 +0800

    feat: save

--- src/main/java/com/anydong/example/springboot/config/ObjectMapperConfig.java
+++ src/main/java/com/anydong/example/springboot/config/ObjectMapperConfig.java
@@ -1,7 +1,10 @@
 package com.anydong.example.springboot.config;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,7 +18,11 @@ public class ObjectMapperConfig {
     @Bean(name = "objectMapper")
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.registerModule(new SimpleModule());
         objectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_ARRAY_AS_NULL_OBJECT, true);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         return objectMapper;
     }
 }
